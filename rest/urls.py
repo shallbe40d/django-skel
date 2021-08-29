f1041098f23066d6354c11593020caec030c6f9f
SBAOH <helix@SBAOHs-MacBook-Pro.local>
2021-08-29 15:47:19 +0900

    add html and static assets config

--- rest/urls.py
+++ rest/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls import include, url
 from rest import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,5 +10,4 @@ urlpatterns = [
     url(r'tdd', views.tdd, name='tdd'),
     url(r'json', views.json, name='json'),
     path(r'<str:path>.html', views.wf, name='wf'),
-
-]
+] + static("assets", document_root="static/assets")
--- static/assets/js/pages/datatables.init.js
+++ static/assets/js/pages/datatables.init.js
@@ -1,1 +1,60 @@
-$(document).ready(function(){$("#datatable").DataTable(),$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","excel","pdf","colvis"]}).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),$(".dataTables_length select").addClass("form-select form-select-sm")});
+$(document).ready(function () {
+	$("#datatable").DataTable(), $("#datatable-buttons").DataTable({
+		lengthChange: !1,
+		buttons: ["copy", "excel", "pdf", "colvis"]
+	}).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $(".dataTables_length select").addClass("form-select form-select-sm")
+});
+
+
+$(document).ready(function() {
+	$("#userList").DataTable({
+		order: [[5, 'desc']],
+		ordering: true,
+		pageLength: 20,
+		serverSide: false,
+		select: true,
+		lengthMenu:  [20, 40, 60, 80, 100 ],
+		dom:
+			"<'row'<'col-sm-12 col-md-6'><'col-sm-12 col-md-6'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'l><'col-sm-12 col-md-7'p>>",
+		language: {
+			zeroRecords: "데이터가 존재하지 않습니다.",
+			search: "",
+			searchPlaceholder: "검색어 입력",
+			lengthMenu: "_MENU_ 개 보기",
+			paginate: {
+				first:"맨앞",
+				previous:"이전",
+				next: "다음",
+				last:"맨뒤"
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$("#ipList").DataTable({
+		order: [[0, 'desc']],
+		ordering: true,
+		pageLength: 20,
+		serverSide: false,
+		select: true,
+		lengthMenu:  [20, 40, 60, 80, 100 ],
+		dom:
+			"<'row'<'col-sm-12 col-md-6'><'col-sm-12 col-md-6'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'l><'col-sm-12 col-md-7'p>>",
+		language: {
+			zeroRecords: "설정된 IP 가 없습니다.",
+			search: "",
+			searchPlaceholder: "검색어 입력",
+			lengthMenu: "_MENU_ 개 보기",
+			paginate: {
+				first:"맨앞",
+				previous:"이전",
+				next: "다음",
+				last:"맨뒤"
+			}
+		}
+	});
+});
