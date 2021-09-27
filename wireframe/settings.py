54a1e6e17140b2df7c7d189eea1547ddc68d655d
SBAOH <helix@SBAOHs-MacBook-Pro.local>
2021-09-28 01:30:18 +0900

    host set kcfuturetech.asuscomm.com

--- wireframe/settings.py
+++ wireframe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '892*pm5*@+11s5a82@o^v^lw-sii2xs(brs750e5%lt71*7#b@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kcfuturetech.asuscomm.com']
 
 
 # Application definition
